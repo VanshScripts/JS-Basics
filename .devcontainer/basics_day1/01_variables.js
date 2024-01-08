e3ea0da3ce9e577f84f15901cede6e03be517f80
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-08 21:18:18 +0000

    about the data types that we use in JS.

---
+++ .devcontainer/basics_day1/01_variables.js
@@ -0,0 +1,6 @@
+const ddNumber=23232; //global scope haii const , cant be override.
+let vansh="hero"; // block{}scope only. cant be redeclared within {}.
+var address="rishikesh"; // global scope , can be redeclared.
+state="uttrakhand";
+
+console.table([ddNumber,vansh,address,state]); // table helps to print the variables all together in a tabular form.
---
+++ .devcontainer/basics_day1/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // new version of JS
+let name="vansh" // string DT
+let age=18; //int
+let correct = true; // boolean
+let city // undefined , no value is assigned to the variable
+let bro=null; //given variable is left empty
+
+console.table([name,age,correct,city,bro]);
+
+console.log(typeof bro);
