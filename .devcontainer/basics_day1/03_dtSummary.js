3b64932b1cc37d13a652e0aac59da19a3c1e7aee
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-11 17:32:56 +0000

    basics

---
+++ .devcontainer/basics_day1/03_dtSummary.js
@@ -0,0 +1,25 @@
+// Primitive data types (call by value)
+//string , Number , Boolena , null , identified , symbol
+let name="vansh" // string DT
+let age=18; //int
+let correct = true; // boolean
+let city // undefined , no value is assigned to the variable
+let bro=null; //given variable is left empty
+
+
+
+// Non-primitve ( call by reference)
+// Arrays , object , functions
+
+const myArray=["1","2","3"];
+
+let myObj={
+    name:"vansh",
+    age:'20',
+    gender:"male"
+}
+ let myFunc=function(){
+    console.log("hello brother");
+ }
+ 
+
