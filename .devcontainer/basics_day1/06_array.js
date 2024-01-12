ceab5f5ede3c86b5127059075a204f768d2e5ddd
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-12 18:23:04 +0000

    basics

---
+++ .devcontainer/basics_day1/06_array.js
@@ -0,0 +1,18 @@
+const myArray=[1,2,3,4]     //initialisation
+//console.log(myArray);
+const myArray2= new Array(5,6,7,8)
+//console.log(myArray2);
+
+
+//methods
+myArray.push(5)    //adding more values in the array
+myArray.pop(3)  // removing existing values in the array
+myArray.unshift(90)  //adds the value in the starting index of the array
+myArray.shift() // remove the value from tyhe starting of the array
+//console.log(myArray.includes(78)); //value exists in the array or not , gives boolean
+//console.log(myArray.indexOf(3)); //value at the given index
+const newArr = myArray.join()  //converts the array from object to string
+///console.log(typeof myArray); //object
+//console.log(typeof newArr);  //string
+
+
