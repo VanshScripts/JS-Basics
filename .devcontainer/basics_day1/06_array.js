83a4e89af7edc36b05db30acb9c6662da264dc25
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-17 17:23:58 +0000

    basics

--- .devcontainer/basics_day1/06_array.js
+++ .devcontainer/basics_day1/06_array.js
@@ -1,6 +1,5 @@
-const myArray=[1,2,3,4]     //initialisation
-//console.log(myArray);
-const myArray2= new Array(5,6,7,8)
+//const myArray=[1,2,3,4]     //initialisation
+//const myArray2= new Array(5,6,7,8)
 //console.log(myArray2);
 
 
@@ -11,12 +10,17 @@ myArray.unshift(90)  //adds the value in the starting index of the array
 myArray.shift() // remove the value from tyhe starting of the array
 //console.log(myArray.includes(78)); //value exists in the array or not , gives boolean
 //console.log(myArray.indexOf(3)); //value at the given index
-const newArr = myArray.join()  //converts the array from object to string
+//const newArr = myArray.join()  //converts the array from object to string
 ///console.log(typeof myArray); //object
 //console.log(typeof newArr);  //string
 //console.log(myArray2.slice(1,4)); // returns selected elements in an array, as a new array
-console.log(myArray2.splice(0,3));
-console.log(myArray2);
+//console.log(myArray2.splice(0,3));
+// console.log(myArray2);
+const hero=["raja","baja","gaja"]
+const bhaii=[1,2,3,4]
+hero.push(bhaii)
+console.log(hero);
+
 
 
 
