f1603fc89be1e9c7318044aa80d001da2ec2920d
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-19 10:51:44 +0000

    basics

---
+++ .devcontainer/basics_day1/09_functions.js
@@ -0,0 +1,56 @@
+function vansh(){ // function is defined
+    //console.log("hii , my name is vansh");
+    //console.log("I am a multitasker");
+}
+vansh(); // function is called
+
+
+
+
+
+function addNames(firstName , lastName){     // function with arguments 
+//console.log(firstName + lastName);
+}
+addNames("Vansh", " Sharma") //values provided while calling 
+
+
+
+
+
+function fullName(first , last){ //arguments stored in a variable and is returning the value when calling
+    let name = first + last
+    return name
+
+}
+const name = fullName("Vansh" , " Sharma");
+//console.log("Mera Naam haii - " ,name);
+
+
+
+function isLoggedIn(username) {   //use of string interpolation in functions
+    return `${username} is logged in!`;
+}
+
+//console.log(isLoggedIn("vansh"));
+
+
+
+
+
+
+// If user has not provided any input , in that case we casn check by using if else statement --- 
+function isLoggedIn(username) {  
+    if(!username){
+        console.log("PLEASE ENTER THE USERNAME");
+        return
+    }
+    return `${username} is logged in!`;
+}
+console.log(isLoggedIn("vansh"));
+
+
+    
+
+
+
+
