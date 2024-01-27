d30be70e383ea598587cd9beb5f334936f97cf95
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-27 18:41:14 +0000

    basics

---
+++ controlFlow/switchCase.js
@@ -0,0 +1,16 @@
+const vanshheronumber = 1
+
+switch (vanshheronumber) {
+    case 69:
+        console.log("He is not  a hero ");
+        break;
+    case 2:
+        console.log("He is not  a hero ");
+        break;
+    case 1:
+        console.log("He is a MANIAC ");
+        break;
+
+    default:
+        break;
+}
