dbfaeb7256a4a0037abd77eabd41ed7c048dbf95
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-27 19:27:34 +0000

    basics

---
+++ controlFlow/truthyValues.js
@@ -0,0 +1,14 @@
+//truthy values
+// - "0" , 'false' , " " , {} , [] , function(){}
+ 
+//falsy values 
+// - false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN 
+
+
+const name = ""
+if (name) {
+    console.log("babu meri haii");
+    
+} else {
+    console.log("babu meri hi rahegi");
+}
