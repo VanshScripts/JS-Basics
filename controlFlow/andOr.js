b0ec866cf9f4529aed6c6e65ff59a275e9c15ff0
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-27 18:25:47 +0000

    basics

---
+++ controlFlow/andOr.js
@@ -0,0 +1,10 @@
+const vanshwhere= true
+const vanshhome= true
+const vanshgone= false
+const vanshrome= false
+if (vanshwhere && vanshhome) {
+    console.log("It is an AND statement. Both of the condition should be true.");
+    
+} else if(vanshhome || vanshrome) {
+    console.log(" It is an OR statement where either of the condition should be true");   
+}
---
+++ controlFlow/ifElse.js
@@ -0,0 +1,20 @@
+/*if (condition) {
+    
+} else {
+    
+}
+*/
+
+// nested if else
+const balance=4 
+if (balance > 32) {
+  console.log("balance is high");
+
+}
+else if(balance==4){
+    console.log("Everything is fine");
+}
+else if(balance<=4){
+    console.log("shaka laka boooom!!!");
+
+}
