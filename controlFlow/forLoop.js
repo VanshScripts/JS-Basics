1424ca2a9b8c2c9cc6c6e9f99d6d5a9e9f310ded
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-28 17:57:52 +0000

    basics

---
+++ controlFlow/forLoop.js
@@ -0,0 +1,13 @@
+for (let i = 0; i <=5; i++) {
+    //console.log(`Loop is working : ${i}`);;
+    
+}
+
+//Nested Loop
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j <10;j++) {
+        //console.log("Inner Looop working");;
+        
+    }
+    //console.log("outer");
+}
