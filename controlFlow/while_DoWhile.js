286f12e8f7a3123e227f6e18a2c01a0d903b8ce7
VanshScripts <156000700+VanshScripts@users.noreply.github.com>
2024-01-29 13:59:37 +0000

    basics

---
+++ controlFlow/while_DoWhile.js
@@ -0,0 +1,25 @@
+let balance = 0
+while (balance<10) {
+    //console.log(`Balance amount is ${balance}`);
+    balance = balance + 1
+}
+
+
+let myArray=[1,2,3,4,5,6]
+let arr=0
+while (arr<myArray.length) {
+    //console.log(`my number is ${myArray[arr]}`);
+    arr = arr +1
+}
+
+
+// Do while loop
+let score = 11
+do {
+ console.log(`Your score is ${score}`);
+ score++
+} while (score<=10);
+
+
+
+// Difference between while and do while loop is that in while the condition is checked first and the program is executed after it. Whereas in do while loop , the program is executed first and after it the condition is checked.
